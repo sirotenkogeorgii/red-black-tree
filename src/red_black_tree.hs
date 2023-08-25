cc7c617027506567a98b59356ccfc02b9e94066c
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-25 14:49:47 +0200

    fix bug in restore_insert function

--- src/red_black_tree.hs
+++ src/red_black_tree.hs
@@ -8,16 +8,13 @@
 -- (Conclusion) If a node N has exactly one child, it must be a red child, because if it were black, its NIL 
 --     descendants would sit at a different black depth than N's NIL child, violating requirement 4.
 
--- INSERTION:
--- l1
--- l2
--- l3
--- l4
--- l5
--- l6
+data Color = RED | BLACK deriving (Show, Eq) 
+data Tree a = NIL | Node (Tree a) Color a (Tree a) deriving (Show) 
 
-data Color = RED | BLACK
-data Tree a = NIL | Node (Tree a) Color a (Tree a)
+recolor_root NIL = NIL
+recolor_root tree@(Node left color val right)
+    | color == RED = (Node left BLACK val right)
+    | otherwise = tree
 
 resore_insert (Node NIL color val NIL) = (Node NIL color val NIL)
 
@@ -26,23 +23,17 @@ restore_insert (Node (Node left_p RED p (Node left_x RED x right_x)) BLACK g (No
 restore_insert (Node (Node left_p RED p right_p) BLACK g (Node (Node left_x RED x right_x) RED u right_u)) = (Node (Node left_p BLACK p right_p) RED g (Node (Node left_x RED x right_x) BLACK u right_u))
 restore_insert (Node (Node left_p RED p right_p) BLACK g (Node left_u RED u (Node left_x RED x right_x))) = (Node (Node left_p BLACK p right_p) RED g (Node left_u BLACK u (Node left_x RED x right_x)))
 
-restore_insert (Node (Node (Node left_x RED x right_x) RED p right_p) BLACK g (Node left_u BLACK u right_u)) = (Node (Node left_x RED x right_x) BLACK p (Node right_p RED g (Node left_u BLACK u right_u)))
-restore_insert (Node (Node left_p RED p (Node left_x RED x right_x)) BLACK g (Node left_u BLACK u right_u)) = (Node (Node left_p RED p left_x) BLACK x (Node right_x RED g (Node left_u BLACK u right_u)))
-restore_insert (Node (Node left_u BLACK u right_u) BLACK g (Node left_p RED p (Node left_x RED x right_x))) = (Node (Node (Node left_u BLACK u right_u) RED g left_p) BLACK p (Node left_x RED x right_x))
-restore_insert (Node (Node left_u BLACK u right_u) BLACK g (Node (Node left_x RED x right_x) RED p right_p)) = (Node (Node (Node left_u BLACK u right_u) RED g left_x) BLACK x (Node right_x RED p right_p))
+restore_insert (Node (Node (Node left_x RED x right_x) RED p right_p) BLACK g uncle) = (Node (Node left_x RED x right_x) BLACK p (Node right_p RED g uncle))
+restore_insert (Node (Node left_p RED p (Node left_x RED x right_x)) BLACK g uncle) = (Node (Node left_p RED p left_x) BLACK x (Node right_x RED g uncle))
+restore_insert (Node uncle BLACK g (Node left_p RED p (Node left_x RED x right_x))) = (Node (Node uncle RED g left_p) BLACK p (Node left_x RED x right_x))
+restore_insert (Node uncle BLACK g (Node (Node left_x RED x right_x) RED p right_p)) = (Node (Node uncle RED g left_x) BLACK x (Node right_x RED p right_p))
 
 restore_insert tree = tree
 
-insert NIL val = (Node NIL RED val NIL)
-insert tree@(Node left color parent_val right) val
-    | parent_val < val = restore_insert (Node left color parent_val (insert right val))
-    | parent_val > val = restore_insert (Node (insert left val) color parent_val right)
+insert_helper NIL val = (Node NIL RED val NIL)
+insert_helper tree@(Node left color parent_val right) val
+    | parent_val < val = restore_insert (Node left color parent_val (insert_helper right val))
+    | parent_val > val = restore_insert (Node (insert_helper left val) color parent_val right)
     | otherwise = tree
 
-
-
--- insert tree@(Node NIL BLACK parent_val NIL) val
---     | parent_val < val = (Node NIL BLACK parent_val (Node NIL RED val NIL))
---     | parent_val > val = (Node (Node NIL RED val NIL) BLACK parent_val NIL)
---     | otherwise = tree
-
+insert tree val = recolor_root (insert_helper tree val)
