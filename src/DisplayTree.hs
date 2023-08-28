aa8a02fc64afe10572f6a979c937bea90db960a0
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-29 01:42:19 +0200

    add double black in Tree data

--- src/DisplayTree.hs
+++ src/DisplayTree.hs
@@ -1,8 +1,11 @@
 module DisplayTree where
 import Data.List
 
+-- TODO: change <$> for map
+-- TODO: change . for for (f())
+
 data Color = RED | BLACK deriving (Show, Eq) 
-data Tree a = NIL | Node (Tree a) Color a (Tree a) deriving (Show) 
+data Tree a = NIL | Node (Tree a) Color a (Tree a) | DoubleBlackNode (Tree a) deriving (Show,Eq)
 data ParentSide = LeftSide | RightSide | Root deriving (Show,Eq) -- no parent for the root
 
 my_zip xs [] = xs
@@ -16,14 +19,9 @@ process_row parentDirection color value parentPosition level = foldl (my_zip) ""
                                                                         LeftSide  -> "┌──"
                                                                         
 
-display_helper _ _ _ NIL = [] -- if we have an empty tree
+display_helper _ _ _ NIL = []
 display_helper parentDirection parentPosition level (Node l color value r) = (display_helper LeftSide nextParentPosition1 (level+1) r) ++ [process_row parentDirection color value parentPosition level] ++ (display_helper RightSide nextParentPosition2 (level+1) l)
                                   where nextParentPosition1 = if parentDirection == RightSide then parentPosition ++ [level] else parentPosition
                                         nextParentPosition2 = if parentDirection == LeftSide then parentPosition ++ [level] else parentPosition
 
 display t = putStr $ (intercalate "\r\n" (display_helper Root [] 0 t)) ++ "\r\n"
-
-main :: IO ()
-main = do
-    let tree = Node (Node (Node NIL BLACK 9 NIL) RED 12 (Node NIL BLACK 17 NIL)) BLACK 18 (Node (Node NIL BLACK 20 NIL) RED 27 (Node NIL BLACK 32 (Node NIL RED 42 NIL)))
-    display tree
