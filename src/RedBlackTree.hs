0802245ebf26a92696f4d6d07c71c27386aeb57c
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-08-29 01:41:38 +0200

    add delete function

--- src/RedBlackTree.hs
+++ src/RedBlackTree.hs
@@ -8,14 +8,18 @@
 import DisplayTree
 
 -- data Color = RED | BLACK deriving (Show, Eq) 
--- data Tree a = NIL | Node (Tree a) Color a (Tree a) deriving (Show) 
+-- data Tree a = NIL | Node (Tree a) Color a (Tree a) | DoubleBlackNode (Tree a) deriving (Show,Eq)
+-- -- data DoubleBlackNode = Bool Tree
 
-recolor_root NIL = NIL
-recolor_root tree@(Node left color val right)
-    | color == RED = (Node left BLACK val right)
-    | otherwise = tree
+--TODO: probably add double black data type
 
-resore_insert (Node NIL color val NIL) = (Node NIL color val NIL)
+get_color NIL = BLACK
+get_color (Node _ color _ _) = color
+
+recolor NIL _ = NIL
+recolor (Node left _ val right) new_color = (Node left new_color val right)
+
+resore_insert (Node NIL color val NIL) = (Node NIL color val NIL) 
 
 restore_insert (Node (Node (Node left_x RED x right_x) RED p right_p) BLACK g (Node left_u RED u right_u)) = (Node (Node (Node left_x RED x right_x) BLACK p right_p) RED g (Node left_u BLACK u right_u))
 restore_insert (Node (Node left_p RED p (Node left_x RED x right_x)) BLACK g (Node left_u RED u right_u)) = (Node (Node left_p BLACK p (Node left_x RED x right_x)) RED g (Node left_u BLACK u right_u))
@@ -35,7 +39,7 @@ insert_helper tree@(Node left color parent_val right) val
     | parent_val > val = restore_insert (Node (insert_helper left val) color parent_val right)
     | otherwise = tree
 
-insert tree val = recolor_root (insert_helper tree val)
+insert tree val = recolor (insert_helper tree val) BLACK
 
 
 contains NIL _ = False
@@ -43,3 +47,83 @@ contains (Node left _ val right) key
     | val < key = contains right key
     | val > key = contains left key
     | otherwise = True
+
+-- NOTE: maybe we should restore insert, becouse parent color can create sequative red nodes
+restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node slib_left BLACK slib_val (Node r_left RED r_val r_right))) =
+        Node (Node replacing_node parent_color parent_val slib_left) BLACK slib_val (Node r_left BLACK r_val r_right)
+
+restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node (Node r_left RED r_val r_right) BLACK slib_val slib_right)) = 
+        Node (Node replacing_node parent_color parent_val r_left) BLACK r_val (Node r_right BLACK slib_val slib_right)
+
+restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node slib_left slib_color slib_val slib_right))
+        | slib_color == BLACK && get_color slib_left == BLACK && get_color slib_right == BLACK = 
+            DoubleBlackNode (Node replacing_node parent_color parent_val (Node slib_left RED slib_val slib_right))
+        | otherwise = Node (Node replacing_node BLACK parent_val (recolor slib_left RED)) BLACK slib_val (recolor slib_right BLACK)
+
+restore_delete tree = tree
+
+
+extract_min (Node (Node left color val right) parent_color parent_val slib_branch)
+    | left == NIL && color == BLACK && get_color right == BLACK =  (restore_delete (Node (DoubleBlackNode right) parent_color parent_val slib_branch), val)
+    | left == NIL && color /= get_color right =  (recolor right BLACK, val)
+    | otherwise = (restore_delete (Node new_tree color val right), min_val)
+    where (new_tree, min_val) = extract_min left
+
+
+delete NIL _ = NIL
+delete tree@(Node left color node_val right) val
+    | node_val < val = Node left color node_val (delete right val)
+    | node_val > val = Node (delete left val) color node_val right
+    | left == NIL && node_val == val = right
+    | right == NIL && node_val == val = left
+    | node_val == val = Node left color new_val new_right
+    | otherwise = tree
+    where (new_right, new_val) = extract_min right
+    
+
+-- Deletion
+-- In Insertion we are solving the problem of consecutive red nodes.
+-- In Deletion we are solving the problem of the number of the black nodes on the pathes.
+-- 1. 
+-- 2.
+-- 3.
+-- 4.
+-- 5.
+-- 6.
+-- 7.
+-- 8.
+
+--                 ┌──19(BLACK)
+--             ┌──18(RED)
+--         ┌──17(BLACK)
+--         |   |   ┌──16(BLACK)
+--         |   └──15(RED)
+--     ┌──14(BLACK)
+--     |   └──13(RED)
+--     |       └──12(BLACK)
+--     |           └──11(BLACK)
+-- ───10(RED)
+--     └──9(BLACK)
+--         └──8(BLACK)
+--             └──7(BLACK)
+
+
+
+--         ┌──16(BLACK)
+--     ┌──15(BLACK)
+--     |   └──13(RED)
+--     |       └──12(BLACK)
+--     |           └──11(BLACK)
+-- ───10(RED)
+--     └──9(BLACK)
+--         └──8(BLACK)
+--             └──7(BLACK)
+
+
+
+-- (Node (Node (Node (Node NIL BLACK 7 NIL) BLACK 8 NIL) BLACK 9 NIL) RED 10 (Node (Node (Node (Node NIL BLACK 11 NIL) BLACK 12 NIL) RED 13 NIL) BLACK 14 (Node (Node NIL RED 15 (Node NIL BLACK 16 NIL)) BLACK 17 (Node NIL RED 18 (Node NIL BLACK 19 NIL)))))
+-- (Node (Node (Node (Node NIL BLACK 11 NIL) BLACK 812 NIL) RED 13 NIL) BLACK 14 (Node (Node NIL RED 15 (Node NIL BLACK 16 NIL)) BLACK 17 (Node NIL RED 18 (Node NIL BLACK 19 NIL))))
+-- (Node (Node NIL RED 15 (Node NIL BLACK 16 NIL)) BLACK 17 (Node NIL RED 18 (Node NIL BLACK 19 NIL)))
+-- extract_min (Node (Node NIL RED 15 (Node NIL BLACK 16 NIL)) BLACK 17 (Node NIL RED 18 (Node NIL BLACK 19 NIL)))
+
+-- (Node (Node (Node (Node NIL BLACK 7 NIL) BLACK 8 NIL) BLACK 9 NIL) RED 10 (Node (Node (Node (Node NIL BLACK 11 NIL) BLACK 812 NIL) RED 13 NIL) BLACK 14 (Node (Node NIL RED 15 (Node NIL BLACK 16 NIL))
