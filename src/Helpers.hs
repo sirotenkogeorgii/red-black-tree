400b7abb50dfe20609c58033a854bdaac004f9a3
georgiisirotenko <sirotenkoegor2001@yandex.com>
2023-09-02 13:40:42 +0200

    add comments

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,55 +1,78 @@
 module Helpers where
 
-data Color = RED | BLACK deriving (Show, Eq) 
+
+-- vertex colors. can be only two colors.
+data Color = RED | BLACK deriving (Show, Eq)
+
+-- the tree can be represented as an empty value, or as a regular node, or as a double black node, 
+-- which only matters for restoring invariants after deleting some value.
 data Tree a = NIL | Node (Tree a) Color a (Tree a) | DoubleBlackNode (Tree a) deriving (Show,Eq)
 
+
+-- get the color of the node.
 get_color NIL = BLACK
 get_color (Node _ color _ _) = color
 
--- get_val NIL = NIL
+
+-- get the value of the node.
 get_val (Node _ _ val _) = val
 
+
+-- get the min value of the tree.
 get_min (Node left _ val _)
     | left /= NIL = get_min left
     | otherwise = val
 
+
+-- check if the node has 2 children.
 has2children NIL = False
 has2children (Node left _ _ right)
     | left == NIL || right == NIL = False
     | otherwise = True
 
+
+-- get the child of the mode. Makes sanse only for nodes with only one child.
 get_child NIL = NIL
 get_child (Node left _ _ right)
     | left == NIL = right
     | right == NIL = left
     | otherwise = NIL
 
+
+-- check if two nodes has the same color.
 different_color tree1 tree2
     | get_color tree1 == get_color tree2 = False
     | otherwise = True
 
+
+-- recolor the node with the color as the second parameter.
 recolor NIL _ = NIL
 recolor (Node left _ val right) new_color = (Node left new_color val right)
 recolor (DoubleBlackNode (Node left _ val right)) new_color = DoubleBlackNode (Node left new_color val right)
 
+
+-- set a new value of the node.
 set_val (Node left color _ right) val = (Node left color val right)
 set_val (DoubleBlackNode (Node left color _ right)) val = DoubleBlackNode (Node left color val right)
 
--- NOTE: maybe we should restore insert, becouse parent color can create sequative red nodes
+
+-- 
 restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node slib_left BLACK slib_val (Node r_left RED r_val r_right))) = -- 3.2) (a) (right right case)
         Node (Node replacing_node BLACK parent_val slib_left) parent_color slib_val (Node r_left BLACK r_val r_right)
 
+--
 restore_delete (Node (Node (Node r_left RED r_val r_right) BLACK slib_val slib_right) parent_color parent_val (DoubleBlackNode replacing_node)) = -- 3.2) (a) (left left case)
         Node (Node r_left BLACK r_val r_right) parent_color slib_val (Node slib_right BLACK parent_val replacing_node)
 
-        
+--
 restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node (Node r_left RED r_val r_right) BLACK slib_val slib_right)) = -- 3.2) (a) (righ left case)
         Node (Node replacing_node BLACK parent_val r_left) parent_color r_val (Node r_right BLACK slib_val slib_right)
 
+-- 
 restore_delete (Node (Node slib_left BLACK slib_val (Node r_left RED r_val r_right)) parent_color parent_val (DoubleBlackNode replacing_node)) = -- 3.2) (a) (left right case)
         Node (Node slib_left BLACK slib_val r_left) parent_color r_val (Node r_right BLACK parent_val replacing_node)
 
-
+--
 restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (Node slib_left slib_color slib_val slib_right))
         | parent_color == BLACK && slib_color == BLACK && get_color slib_left == BLACK && get_color slib_right == BLACK = 
             DoubleBlackNode (Node replacing_node BLACK parent_val (Node slib_left RED slib_val slib_right))
@@ -57,6 +80,7 @@ restore_delete (Node (DoubleBlackNode replacing_node) parent_color parent_val (N
             (Node replacing_node BLACK parent_val (Node slib_left RED slib_val slib_right))         
         | otherwise = restore_delete (Node (Node replacing_node BLACK parent_val (recolor slib_left RED)) BLACK slib_val (recolor slib_right BLACK))
 
+-- 
 restore_delete (Node (Node slib_left slib_color slib_val slib_right) parent_color parent_val (DoubleBlackNode replacing_node))
         | parent_color == BLACK && slib_color == BLACK && get_color slib_left == BLACK && get_color slib_right == BLACK = 
             DoubleBlackNode (Node (Node slib_left RED slib_val slib_right) BLACK parent_val replacing_node)
@@ -64,19 +88,30 @@ restore_delete (Node (Node slib_left slib_color slib_val slib_right) parent_colo
             (Node (Node slib_left RED slib_val slib_right) BLACK parent_val replacing_node)
         | otherwise = (Node (recolor slib_left BLACK) BLACK slib_val (Node (recolor slib_right RED) BLACK parent_val replacing_node))
 
+--
 restore_delete tree = tree
 
+
+
+-- we can't delete from an empty tree.
 delete_helper NIL _ = NIL
+
+--
 delete_helper tree@(Node left color node_val right) val
-    | right /= NIL && get_val right == val && not (has2children right) && different_color (get_child right) right = (Node left color node_val (recolor (get_child right) BLACK)) -- 2)
-    | left /= NIL && get_val left == val && not (has2children left) && different_color (get_child left) left = (Node (recolor (get_child left) BLACK) color node_val right) -- 2)
-    | right /= NIL && get_val right == val && not (has2children right) && get_color right == BLACK && get_color (get_child right) == BLACK = restore_delete (Node left color node_val (DoubleBlackNode (get_child right))) -- 3.1,2) 
-    | left /= NIL && get_val left == val && not (has2children left) && get_color left == BLACK && get_color (get_child left) == BLACK = restore_delete (Node (DoubleBlackNode (get_child left)) color node_val right) -- 3.1,2)                                                                                                           
-    | node_val < val = restore_delete (Node left color node_val (delete_helper right val))                                                                                        
-    | node_val > val = restore_delete (Node (delete_helper left val) color node_val right)                                                                                        
-    | otherwise = restore_delete (set_val deleted_min_tree min_val)
+    | right /= NIL && get_val right == val && not (has2children right) && different_color (get_child right) right = 
+            (Node left color node_val (recolor (get_child right) BLACK)) -- simple case when we delete node with one children and the only child has the different color.
+    | left /= NIL && get_val left == val && not (has2children left) && different_color (get_child left) left = 
+            (Node (recolor (get_child left) BLACK) color node_val right) -- simple case when we delete node with one children and the only child has the different color.
+    | right /= NIL && get_val right == val && not (has2children right) && get_color right == BLACK && get_color (get_child right) == BLACK = 
+            restore_delete (Node left color node_val (DoubleBlackNode (get_child right))) -- delete the current node  and the only child has the same black color: replace with child -> make it double black -> restore invariant.     
+    | left /= NIL && get_val left == val && not (has2children left) && get_color left == BLACK && get_color (get_child left) == BLACK = 
+            restore_delete (Node (DoubleBlackNode (get_child left)) color node_val right) -- delete the current node and the only child has the same black color: replace with child -> make it double black -> restore invariant.                                                                                                     
+    | node_val < val = restore_delete (Node left color node_val (delete_helper right val)) -- if the current node value is less -> go right                                                                                    
+    | node_val > val = restore_delete (Node (delete_helper left val) color node_val right) -- if the current node value is greater -> go left                                                                                      
+    | otherwise = restore_delete (set_val deleted_min_tree min_val) -- if delete the current node: change current value by the minimum value in the right subtree and delete it in the right subtree.
     where min_val = get_min right
           deleted_min_tree = delete_helper tree min_val
+
 
 resore_insert (Node NIL color val NIL) = (Node NIL color val NIL) 
 
